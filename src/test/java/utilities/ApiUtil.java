08ea2557f48c2bdabb000430cbb08046c91a4005
AbdullahTest-ato <abdullahbatarfiab@gmail.com>
2025-11-17 20:24:46 +0300

    Fix: post-merge adjustments and bug fixes

--- src/test/java/utilities/ApiUtil.java
+++ src/test/java/utilities/ApiUtil.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
